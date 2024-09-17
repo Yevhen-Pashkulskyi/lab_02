5eb474fc14fce565804d3ad985ce9dc8c0353ae8
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-09-17 17:23:13 +0300

    modified varaibls

--- src/app/SearchPoint.c
+++ src/app/SearchPoint.c
@@ -1,30 +1,29 @@
 //Pashkulskyi Yevhen AS-246
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
 
 int main() {
     // Об'явленні змінних типу float та integer
     float diam, radius, dis;
-    float x, y, point;
+    float x_coordinate, y_coordinate, point;
     int rate = 2;
 
     // Введення даних користувачем
     printf("Input diametr: ");
     scanf("%f", &diam);
     printf("Input \'x\' and \'y\' coordinates separated by space: ");
-    scanf("%f%f", &x, &y);
+    scanf("%f%f", &x_coordinate, &y_coordinate);
 
     // Розрахунок радіуса, сторони квадрата та розрахунок крапки за величиною
     radius = diam / 2;
     dis = radius;
-    point = sqrt(pow(x, rate) + pow(y, rate));
+    point = sqrt(pow(x_coordinate, rate) + pow(y_coordinate, rate));
 
     // умова для визначееня в якій площіні знаходеться крапка
-    if (fabs(x) <= dis && fabs(y) <= dis) {
-        if (x < 0 && y > 0 && fabs(point) >= radius) {
+    if (fabs(x_coordinate) <= dis && fabs(y_coordinate) <= dis) {
+        if (x_coordinate < 0 && y_coordinate > 0 && fabs(point) >= radius) {
             printf("The point is in area \"A\"");
-        } else if (x > 0 && y > 0 && fabs(point) >= radius) {
+        } else if (x_coordinate > 0 && y_coordinate > 0 && fabs(point) >= radius) {
             printf("The point is in area \"B\"");
         } else {
             printf("The point is not in region \"A\" and \"B\"");
